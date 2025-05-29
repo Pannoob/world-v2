159b07af5b6e9fa9bd68d6b3fd3a2700f2c41c7a
Pannoob <213972479+Pannoob@users.noreply.github.com>
2025-05-29 21:00:49 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,8 +2,13 @@
 #include "world.h"
 #include <stdlib.h>
 
-
+// Start of program
 int main(int argc, char** argv){
-    start_world(init_game,game_event,free);
+    // command for changing the size of console
+    char command[50];
+    sprintf(command, "MODE CON COLS=%d LINES=%d", GAME_OFFSET_X*2+MAP_WIDTH*2, GAME_OFFSET_Y*2+MAP_HEIGHT);
+    system(command);
+    // game start
+    start_world(init_game, game_event, free);
     return 0;
 }
